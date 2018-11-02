a7d52399a482beaf76082c2e944c068825e9a493
harsha9726 <harsha9726@gmail.com>
2018-11-03 03:24:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const {mongoose} = require('./db/mongoose');
+var session = require('express-session');
+//var MongoStore = require('connect-mongo')(session);
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//controllers
+var userController = require('./controllers/usercontroller');
+
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false
+}));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+
+app.get('/newAccount', function (request, response) {
+  response.render("Register.ejs");
+});
+
+
+//user authentication
+app.get('/', function (request, response) {
+  response.render("index.ejs");
+});
+
+
+//POST route for updating data
+app.post('/admin', userController.user);
+
+// GET route after registering
+app.get('/profile', userController.userlogin);
+app.get('/admin', userController.userlogin);
+app.get('/userretreive', userController.userretreive);
+
+// GET for logout logout
+app.get('/logout', userController.logout);
+
+//user authentication end
+
+
+app.listen(app.get('port'), process.env.IP, function () {
+
+  console.log("Node Server running at port:" + app.get('port'));
+
+
+});
---
+++ controllers/usercontroller.js
@@ -0,0 +1,97 @@
+const {mongoose} = require('./../db/mongoose');
+const {User} = require('./../models/userschema');
+
+
+exports.user = function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.pass !== req.body.passconf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.pass &&
+    req.body.passconf) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.pass,
+      //passwordConf: req.body.passconf,
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+
+  } else if (req.body.logemail && req.body.logpass) {
+    User.authenticate(req.body.logemail, req.body.logpass, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        if(user.admin){
+          return  res.render('./../views/adminpage',)
+          }
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+}
+exports.userlogin = function (req, res, next) {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return res.redirect('/');
+        } else {
+          if(user.admin){
+            return  res.render('./../views/adminpage',)
+          }
+          return res.render('./../views/profile.ejs');
+        }
+      }
+    });
+}
+
+exports.logout = function (req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+}
+
+exports.userretreive = (req,res) =>{
+  User.find().then((images) => {
+  return  res.render('./../views/adminpage2', {
+      title: 'Images',
+      images: images
+    } );
+  }, (e) => {
+  return  res.status(400).send(e);
+  })
+}
